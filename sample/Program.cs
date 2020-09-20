f250fdb86e2784c9a759515b716c638234d43ee5
Patrick Westerhoff <PatrickWesterhoff@gmail.com>
2020-09-20 20:51:47 +0200

    Add sample with encrypted configuration

---
+++ sample/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EncryptionSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            var configuration = host.Services.GetService<IConfiguration>();
+
+            foreach (var config in configuration.AsEnumerable())
+            {
+                Console.WriteLine($"{config.Key} = {config.Value}");
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.Sources.Clear();
+                    builder.AddEncryptedJsonFile("securesettings.json", "CN=ConfigurationEncryptionExample1");
+                });
+    }
+}
